1ab186442093d06cd60fdfa0df42c15cc44ab879
FeeZBLH <79203450+FeeZBLH@users.noreply.github.com>
2022-01-27 22:34:40 +0100

    Accueil telephone

--- accueil.php
+++ accueil.php
@@ -20,9 +20,15 @@
                 <h1>Le jeu Ogrinami !</h1>
                 <h2>La Terre a besoin de ton aide !</h2>
             </div>
-            <div class="bouton">Bientôt disponible</div>
+            <div class="bouton"><div>Bientôt disponible</div></div>
         </section>
         <section class="rubriques">
+            <div class="titre"><div>Toutes les rubriques</div></div>
+            <div class="liste">
+                <div class="rubrique"><div class="img"></div><div class="bouton"><div>Mini-jeux</div></div></div>
+                <div class="rubrique"><div class="img"></div><div class="bouton"><div>Mini-jeux</div></div></div>
+                <div class="rubrique"><div class="img"></div><div class="bouton"><div>Mini-jeux</div></div></div>
+            </div>
         </section>
     </div>
 
